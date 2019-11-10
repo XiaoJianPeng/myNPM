e06186d7532408b897289e7c05173a8e93b7c104
xiaojianpeng <xiaojianpeng@sishuxuefu.com>
2019-11-10 22:33:15 +0800

    增加异步迭代请求

--- list-iterator/iterator.js
+++ list-iterator/iterator.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 class Iterator {
   constructor(arr, limit) {
     this.arr = arr
@@ -13,7 +15,49 @@ class Iterator {
       this.currentIndex += this.limit
       return result
     }
+  }，
+
+  constructor(api,params,limit){
+    this.page = 1
+    this.limit = limit
+    this.instance = axios.create({
+      baseURL: api,
+      timeout: 10000,
+    })
+    this.next = function(){
+      return this.get(api, params, limit)
+    }
   }
+
+  /**
+   * @param {string} api
+   * @param {object} params
+   * @return
+   * {
+   *  done:boolean,
+   *  total:number,
+   *  value:array
+   * }
+   */
+  async get(api, params) {
+    const result = await this.instance.get(api, {
+      page: this.page,
+      size: this.limit,
+      ...params
+    })
+    const pageData = result.data
+    const total = result.total
+    if (pageData && pageData.length > 0) {
+      currentPage++
+    }
+    const done = pageData.length === 0
+    return {
+      total,
+      done,
+      value: pageData,
+    }
+  }
+
 }
 
 exports.Iterator = Iterator
